94061a11d3ded1b350418315c20c24cf653c29d2
Barba2k2 <lorenzo.dz@hotmail.com>
2024-12-24 17:39:02 -0300

    feat(main): Remove unused configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:lucid_validation/lucid_validation.dart';
 
 import 'src/app_widget.dart';
@@ -8,7 +8,7 @@ import 'src/core/DI/dependency_injector.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load(fileName: '.env');
+  // await dotenv.load(fileName: '.env');
   final culture = Culture('pt', 'BR');
   LucidValidation.global.culture = culture;
 
