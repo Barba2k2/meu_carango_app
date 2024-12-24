268c715beabc257f7e6ec9be32eb972d2ab60d55
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-12-24 18:38:14 -0300

    fix(images): Refactor app_images class and add package name string

--- design_system/lib/design_system.dart
+++ design_system/lib/design_system.dart
@@ -1,3 +1,3 @@
 library design_system;
 
-export 'app_images/images.dart';
+export 'image/image.dart';
--- design_system/lib/image/app_image.dart
+++ design_system/lib/image/app_image.dart
@@ -1,4 +1,6 @@
-class AppImages {
+class AppImage {
+  static const String packageName = 'design_system';
+  
   static const String logoDark = 'assets/logo/dark_logo.svg';
   static const String logoLight = 'assets/logo/light_logo.svg';
 }
