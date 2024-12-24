780537482a6ca369ce90a819fd31dd11ebc8c850
Barba2k2 <lorenzo.dz@hotmail.com>
2024-12-24 17:38:09 -0300

    feat(core): Add intial pages

---
+++ lib/src/core/features/splash/presentation/pages/splash_page.dart
@@ -0,0 +1,43 @@
+import 'package:design_system/design_system.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToOnboarding();
+  }
+
+  _navigateToOnboarding() async {
+    await Future.delayed(const Duration(seconds: 6));
+    if (mounted) {
+      context.go('/onboarding');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.deepPurple,
+        child: Center(
+          child: Image.asset(
+            AppImages.logoDark,
+            width: 130,
+            height: 130,
+          ),
+        ),
+      ),
+    );
+  }
+}
