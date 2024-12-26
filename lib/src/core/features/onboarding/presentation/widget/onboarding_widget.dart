9fb21f0766fbcebd8f4994c175b0f4aea4154c94
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-12-25 23:42:34 -0300

    feat(onboarding): Add dynamic image and titles

---
+++ lib/src/core/features/onboarding/presentation/widget/onboarding_widget.dart
@@ -0,0 +1,54 @@
+import 'package:design_system/design_system.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+
+import '../data/onboarding_data.dart';
+
+class OnboardingWidget extends StatelessWidget {
+  final OnboardingData data;
+  final int currentIndex;
+
+  const OnboardingWidget({
+    super.key,
+    required this.data,
+    required this.currentIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final double imageHeight = currentIndex == 2 ? 380 : 214;
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset(
+          data.image,
+          width: 330,
+          height: imageHeight,
+          package: AppImage.packageName,
+          fit: BoxFit.contain,
+        ),
+        const Gap(64),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 12,
+          children: [
+            Text(
+              data.title,
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            Text(
+              data.description,
+              overflow: TextOverflow.clip,
+              style: Theme.of(context).textTheme.bodyLarge,
+              maxLines: 5,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
