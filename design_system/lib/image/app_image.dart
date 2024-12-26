7f086efae49f74f12b534e6acedcebb2828cf457
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-12-25 21:14:15 -0300

    feat(design_system): Add onboarding iimages on AppImages class

--- design_system/lib/image/app_image.dart
+++
@@ -1,6 +0,0 @@
-class AppImage {
-  static const String packageName = 'design_system';
-  
-  static const String logoDark = 'assets/logo/dark_logo.svg';
-  static const String logoLight = 'assets/logo/light_logo.svg';
-}
---
+++ design_system/lib/src/image/app_image.dart
@@ -0,0 +1,12 @@
+class AppImage {
+  static const String packageName = 'design_system';
+
+  // Logo
+  static const String logoDark = 'assets/logo/dark_logo.svg';
+  static const String logoLight = 'assets/logo/light_logo.svg';
+
+  // Onboarding
+  static const String onboarding1 = 'assets/onboarding/onboarding1.svg';
+  static const String onboarding2 = 'assets/onboarding/onboarding2.svg';
+  static const String onboarding3 = 'assets/onboarding/onboarding3.svg';
+}
