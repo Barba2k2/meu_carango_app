30e8bde41db8c2cd26e83b4622c78a581710f337
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-12-25 21:14:48 -0300

    feat(theme): Add AppColors on design_system

---
+++ design_system/lib/src/theme/app_colors.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+class AppColors {
+  // Primary Colors
+  static const Color primaryColor = Color(0xFFA100FF);
+  static const Color secondaryColor = Color(0xFF34C1CC);
+
+  // Text Colors
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+
+  // Status Colors
+  static const Color successColor = Color(0xff00C933);
+  static const Color errorColor = Color(0xffFF0004);
+  static const Color alertColor = Color(0xffFFBB00);
+
+  // Bg Colors
+  static const Color darkBgColor = Color(0xFF222222);
+  static const Color whiteBgColor = Color(0xFFFFFFFF);
+
+  // Selected Colors
+  static const Color blackSelectedColor = Color(0xFF311E3D);
+  static const Color whiteSelectedColor = Color(0xFFF3E0FF);
+
+  // Butons Bg
+  static const Color adotarButtonColor = Color(0xFF34C1CC);
+  static const Color doarButtonColor = Color(0xFFFF5A79);
+  static const Color textGreyColor = Color(0xFF767676);
+}
