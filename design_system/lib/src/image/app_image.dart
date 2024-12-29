ebb7a3a2a0f439b790dee884c3aa61be440d546d
Barba2k2 <lorenzo.dz@hotmail.com>
2024-12-28 21:02:50 -0300

    feat(design_system): Add image path on AppImage

--- design_system/lib/src/image/app_image.dart
+++ design_system/lib/src/image/app_image.dart
@@ -7,8 +7,13 @@ class AppImage {
   static const String logoText = 'assets/logo/text_logo.svg';
   static const String logoTextDark = 'assets/logo/text_logo_dark.svg';
 
-  // Onboarding
+  // Onboarding - Light
   static const String onboarding1 = 'assets/onboarding/on-boarding-image-01.svg';
   static const String onboarding2 = 'assets/onboarding/on-boarding-image-02.svg';
   static const String onboarding3 = 'assets/onboarding/on-boarding-image-03.svg';
+
+  // Onboarding -- Dark
+  static const String onboardingBlack1 = 'assets/onboarding/on-boarding-image-black-01.svg';
+  static const String onboardingBlack2 = 'assets/onboarding/on-boarding-image-black-02.svg';
+  static const String onboardingBlack3 = 'assets/onboarding/on-boarding-image-black-03.svg';
 }
