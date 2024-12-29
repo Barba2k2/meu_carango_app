9d1f926991064cf65bc3beff7ffa66112e8e9913
Barba2k2 <lorenzo.dz@hotmail.com>
2024-12-29 19:06:25 -0300

    feat(design_system): Update design_system

--- design_system/lib/design_system.dart
+++ design_system/lib/design_system.dart
@@ -5,7 +5,7 @@ export './src/buttons/buttons.dart';
 // export './src/drawer/drawer_ds.dart';
 // export './src/icons/icons.dart';
 export './src/image/image.dart';
-// export './src/inputs/inputs.dart';
+export './src/inputs/inputs.dart';
 // export './src/sliders/sliders.dart';
 export './src/theme/theme.dart';
 // export 'src/banner/banner_ds.dart';
