b11c3788825600a6f79d1c9cd2dd37b359c1a586
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-12-29 23:28:08 -0300

    feat(app_image): Refactors asset path management with base constants

--- design_system/lib/src/image/app_image.dart
+++ design_system/lib/src/image/app_image.dart
@@ -1,26 +1,32 @@
 class AppImage {
   static const String packageName = 'design_system';
 
+  // Base path
+  static const String iconsPath = 'assets/icons';
+  static const String loginPath = 'assets/login';
+  static const String logosPath = 'assets/logo';
+  static const String onboardingPath = 'assets/onboarding';
+
   // Logo
-  static const String logoDark = 'assets/logo/dark_logo.svg';
-  static const String logoLight = 'assets/logo/light_logo.svg';
-  static const String logoText = 'assets/logo/text_logo.svg';
-  static const String logoTextDark = 'assets/logo/text_logo_dark.svg';
+  static const String logoDark = '$logosPath/dark_logo.svg';
+  static const String logoLight = '$logosPath/light_logo.svg';
+  static const String logoText = '$logosPath/text_logo.svg';
+  static const String logoTextDark = '$logosPath/text_logo_dark.svg';
 
   // Icons
-  static const String googleIcon = 'assets/icons/google.svg';
+  static const String googleIcon = '$iconsPath/google_icon.svg';
 
   // Onboarding - Light
-  static const String onboarding1 = 'assets/onboarding/on-boarding-image-01.svg';
-  static const String onboarding2 = 'assets/onboarding/on-boarding-image-02.svg';
-  static const String onboarding3 = 'assets/onboarding/on-boarding-image-03.svg';
+  static const String onboarding1 = '$onboardingPath/on-boarding-image-01.svg';
+  static const String onboarding2 = '$onboardingPath/on-boarding-image-02.svg';
+  static const String onboarding3 = '$onboardingPath/on-boarding-image-03.svg';
 
   // Onboarding -- Dark
-  static const String onboardingBlack1 = 'assets/onboarding/on-boarding-image-black-01.svg';
-  static const String onboardingBlack2 = 'assets/onboarding/on-boarding-image-black-02.svg';
-  static const String onboardingBlack3 = 'assets/onboarding/on-boarding-image-black-03.svg';
+  static const String onboardingBlack1 = '$onboardingPath/on-boarding-image-black-01.svg';
+  static const String onboardingBlack2 = '$onboardingPath/on-boarding-image-black-02.svg';
+  static const String onboardingBlack3 = '$onboardingPath/on-boarding-image-black-03.svg';
 
   // Login
-  static const String login = 'assets/login/login.svg';
-  static const String loginDark = 'assets/login/login-dark.svg';
+  static const String login = '$loginPath/login.svg';
+  static const String loginDark = '$loginPath/login-dark.svg';
 }
